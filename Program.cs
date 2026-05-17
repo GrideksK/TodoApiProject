5ccd929ffdb8c4ce965bb71a0cc793b1093581b4
GrideksK <grideks@yandex.ru>
2026-05-18 00:18:56 +0300

    Refactor: connection string selection and add frontend UI

--- Program.cs
+++ Program.cs
@@ -2,10 +2,13 @@ using Microsoft.EntityFrameworkCore;
 using TodoApi.Models;
 using TodoApi.Services;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<TodoContext>(opt => opt.UseSqlite("Data Source =Data/TodoDto.db"));
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+
+builder.Services.AddDbContext<TodoContext>(opt => opt.UseSqlite(connectionString));
+
 builder.Services.AddScoped<ITodoService, TodoService>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -16,15 +19,19 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
 app.MapControllers();
+
 
 using (var scope = app.Services.CreateScope())
 {
